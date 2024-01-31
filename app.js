f0c23df60e00a68085555cf600849a3fef827a1f
git <lduart441@west-mec.org>
2024-01-31 16:32:31 -0700

    added all project files

---
+++ app.js
@@ -0,0 +1,9 @@
+const blackCoffee = document.getElementById("blackCoffee");
+const espressoCofee = document.getElementById("espressoCof");
+const mochaCoffee = document.getElementById("mochaCof");
+const latteCoffee = document.getElementById("latteCof");
+const frapCoffee = document.getElementById("frapCof");
+
+function coffeeAlert(){
+    alert("SORRY COFFEE MACHINE IS BROKEN...")
+}
